eb8094a9c387df880a3f1f7a5f9c0dfb42a9f5df
Dario L Garcia <dario.garcia@10pines.com>
2017-06-22 21:29:13 -0300

    Ejecutar acciones desde dropdown

--- app/components/multi-accion.js
+++ app/components/multi-accion.js
@@ -6,6 +6,22 @@ export default Ember.Component.extend(BaseGrafoServiceInjected, Sizeable, {
 
   accionElegida: null,
 
+  noSePuedeEjecutar: Ember.computed('accionElegida', 'accionElegida.parametros.@each.valor',function () {
+    let accionElegida = this._accionElegida();
+    if(!accionElegida){
+      return true;
+    }
+    let parametros = this._parametros();
+    for (var i = 0; i < parametros.length; i++) {
+      var parametro = parametros[i];
+      let valor = parametro.get('valor');
+      if(valor === undefined){
+        return true;
+      }
+    }
+    return false;
+  }),
+
   didInsertElement(){
     this._super(...arguments);
     this.baseGrafoService().buscarAccionesDisponibles()
@@ -16,7 +32,32 @@ export default Ember.Component.extend(BaseGrafoServiceInjected, Sizeable, {
 
   actions: {
     ejecutarAccion: function () {
-      this.set('mensajeDeFeedback', 'Elegida: ' + this.get('accionElegida.nombre'));
+      let recurso = this.get('accionElegida.recurso');
+      let parametros = this._calcularObjetoParametros();
+      this.baseGrafoService().ejecutarAccion(recurso, parametros)
+        .then(()=>{
+          this.set('mensajeDeFeedback', 'Hecho');
+        });
     }
+  },
+
+  _accionElegida() {
+    return this.get('accionElegida');
+  },
+  _parametros() {
+    return this.get('accionElegida.parametros');
+  },
+
+  _calcularObjetoParametros(){
+    let objeto = Ember.Object.create();
+    let parametros = this._parametros();
+    for (var i = 0; i < parametros.length; i++) {
+      var parametro = parametros[i];
+      let valor = parametro.get('valor');
+      let propiedad = parametro.get('nombre');
+      objeto.set(propiedad, valor);
+    }
+    return objeto;
   }
+
 });
